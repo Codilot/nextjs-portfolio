b3f7cc1091681bbf170d43a344b2992046cc3fc5
codilot <30231189+Codilot@users.noreply.github.com>
2020-12-18 18:42:39 +0100

    Quick and dirty landing page

---
+++ components/Head.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+const HeadComp = () => (
+    <Head>
+        <title>Eveline Kooijman | Designer | Developer</title>
+        <meta charset="utf-8" />
+        <meta
+            name="description"
+            content="Eveline Kooijman designs / develops interfaces, prototypes and design systems."
+        />
+        <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+            key="device"
+        />
+        <meta
+            property="og:title"
+            content="Eveline Kooijman designs / develops interfaces, prototypes and design systems."
+            key="title"
+        />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+)
+
+export default HeadComp
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Button from '../components/Button'
+
+const About = () => (
+    <div>
+        <h1>About page</h1>
+        <Button></Button>
+    </div>
+)
+
+export default About
